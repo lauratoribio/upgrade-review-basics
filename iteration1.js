44d6ee1c374b8295a82ba5e2beacde76a6a9c974
Laura Toribio <lsancheztoribio@hotmail.com>
2021-12-09 09:03:42 +0100

    Iteration#1 not solved

---
+++ iteration1.js
@@ -0,0 +1,20 @@
+//usa forof para recorrer el array de películas, genera un nuevo array con las categorías de las películas e imprime por consola el array de categorías. 
+//Ten en cuenta que las categorías no deberían repetirse. Para filtrar las categorías puedes ayudarte de la función .includes()
+
+
+const movies = [
+    {title: 'Madaraspar', duration: 192, categories: ['comedia', 'aventura']},
+    {title: 'Spiderpan', duration: 122, categories: ['aventura', 'acción']},
+    {title: 'Solo en Whatsapp', duration: 223, categories: ['comedia', 'thriller']},
+    {title: 'El gato con guantes', duration: 111, categories: ['comedia', 'aventura', 'animación']},
+]
+
+const movieCategories = []
+
+for(let object of movies) {
+    if(!movieCategories.includes(object.categories)) {
+        movieCategories.push(object.categories)
+    }
+}
+console.log(movieCategories)
+
